557481bdbbc18716d51fa9333727b086ed86c239
Doug-Moody <40870695+Doug-Moody@users.noreply.github.com>
2020-01-17 11:07:06 -0500

    Update Windows10_Security_Patches_To_Jan-14-2020.ps1

--- Windows10_Security_Patches_To_Jan-14-2020.ps1
+++ Windows10_Security_Patches_To_Jan-14-2020.ps1
@@ -8,55 +8,29 @@ if (![System.IO.Directory]::Exists("C:\support\")) {
     [system.io.directory]::CreateDirectory("c:\Support\Updates")
 }
 
+
+# Check Bitness of OS
+if ((gwmi win32_operatingsystem | select osarchitecture).osarchitecture -eq "64-bit") {
+    #64 bit logic here
+    Write "64-bit OS Detected" | Out-file -Filepath c:\support\Updates\$env:computername.hotfixes.txt -Append
+    $Script:BIT = "64"
+}
+else {
+    #32 bit logic here
+    Write "32-bit OS Detected" | Out-file -Filepath c:\support\Updates\$env:computername.hotfixes.txt -Append
+    $Script:BIT = "86"
+
+}
+
+
 # Check if windows 10 before continuing to patch
 Set-Variable -Name "WINVS" -Value ([Environment]::OSVersion.Version).Major
-
-#Matching OS Build to a KB Identifier & Downloads
-switch ((Get-CimInstance Win32_OperatingSystem).BuildNumber) {
-    #Windows 7 & 8 
-    7600 { $Script:OS = "W2K8R2" }
-    7601 { $Script:OS = "W2K8R2SP1" }    
-    9200 { $Script:OS = "W2K12" }
-    9600 { $Script:OS = "W2K12R2" }
-    #Win 10 versions
-    10240 { $Script:OS = "W2K10v1507" }
-    10586 { $Script:OS = "W2K10v1511" }
-    14393 { $Script:OS = "W2K10v1607" }
-    14393 { $Script:KB = "KB4534271" }
-    14393 { $Script:KB64 = "kb4534271-x64_a009e866038836e277b167c85c58bbf1e0cc5dc8.msu" }
-    14393 { $Script:KBx86 = "kb4534271-x86_1401cdaf3781a6b032b558afd90fff6faa5569d3.msu" }
-    15063 { $Script:OS = "W2K10v1703" }
-    15063 { $Script:KB64 = "W2K10v1703" }
-    15063 { $Script:KB86 = "W2K10v1703" }
-    16299 { $Script:OS = "W2K10v1709" }
-    16299 { $Script:KB64 = "W2K10v1709" }
-    16299 { $Script:KB86 = "W2K10v1709" }
-    17134 { $Script:OS = "W2K10v1803" }
-    17134 { $Script:KB64 = "W2K10v1803" }
-    17134 { $Script:KB86 = "W2K10v1803" }
-    17763 { $Script:OS = "W2K10v1809" }
-    17763 { $Script:KB64 = "W2K10v1809" }
-    17763 { $Script:KB86 = "W2K10v1809" }
-    18362 { $Script:OS = "W2K10v1903" }
-    18362 { $Script:KB = "KB4528760" }
-    18362 { $Script:1903 = "W2K10v1903" }
-    18362 { $Script:1903x86 = "W2K10v1903" }
-    18363 { $Script:OS = "W2K10v1909" }
-    18363 { $Script:KB = "KB4528760" } 
-    18363 { $Script:1909 = "W2K10v1909" }
-    18363 { $Script:1909x86 = "W2K10v1909" }
-    19041 { $Script:OS = "W2K10v2004" }
-    19041 { $Script:2004 = "W2K10v2004" }
-    19041 { $Script:2004x86 = "W2K10v2004" }
-    # PENDING: Server 2016
-    default { $Script:OS = "Not Listed" }
-}
 
 # Logging OS Version info
 $OS | Out-file -Filepath c:\support\Updates\$env:computername.hotfixes.txt
 Get-HotFix | Select HotFixID, InstalledOn | Out-file -Filepath c:\support\Updates\$env:computername.hotfixes.txt -Append
 
-# Exit if OS is not Win10
+# Exit if OS is not Win10  Can remove email section if not needed
 If (!($WINVS -eq 10)) {
     #Pending further email info
     Send-MailMessage -from "ServiceEmail <PatcherEmail@Yourdomain.com>"
@@ -69,35 +43,66 @@ If (!($WINVS -eq 10)) {
 }
 
 
-#Alert user that system is currently updating
-msg console /server:localhost "Hello , your computer needs security patches and will need to remain powered on until complete. You can continue using the system but prepare by saving data as there will be a need to reboot later"
-Set-Variable -Name "BUILD" -Value ([Environment]::OSVersion.Version).Build
+    #Matching OS Build to a KB Identifier & Downloads
+    switch ((Get-CimInstance Win32_OperatingSystem).BuildNumber) {
+        #Windows 7 & 8 
+        7600 { $Script:OS = "W2K8R2" }
+        7601 { $Script:OS = "W2K8R2SP1" }    
+        9200 { $Script:OS = "W2K12" }
+        9600 { $Script:OS = "W2K12R2" }
+        #Win 10 versions
+        10240 { $Script:OS = "W2K10v1507" }
+        10586 { $Script:OS = "W2K10v1511" }
+        14393 { $Script:OS = "W2K10v1607" }
+        14393 { $Script:KB = "KB4534271" }
+        14393 { $Script:KB64 = "kb4534271-x64_a009e866038836e277b167c85c58bbf1e0cc5dc8.msu" }
+        14393 { $Script:KBx86 = "kb4534271-x86_1401cdaf3781a6b032b558afd90fff6faa5569d3.msu" }
+        15063 { $Script:OS = "W2K10v1703" }
+        15063 { $Script:KB64 = "W2K10v1703" }
+        15063 { $Script:KB86 = "W2K10v1703" }
+        16299 { $Script:OS = "W2K10v1709" }
+        16299 { $Script:KB64 = "W2K10v1709" }
+        16299 { $Script:KB86 = "W2K10v1709" }
+        17134 { $Script:OS = "W2K10v1803" }
+        17134 { $Script:KB64 = "W2K10v1803" }
+        17134 { $Script:KB86 = "W2K10v1803" }
+        17763 { $Script:OS = "W2K10v1809" }
+        17763 { $Script:KB64 = "W2K10v1809" }
+        17763 { $Script:KB86 = "W2K10v1809" }
+        18362 { $Script:OS = "W2K10v1903" }
+        18362 { $Script:KB = "KB4528760" }
+        18362 { $Script:1903 = "W2K10v1903" }
+        18362 { $Script:1903x86 = "W2K10v1903" }
+        18363 { $Script:OS = "W2K10v1909" }
+        18363 { $Script:KB = "KB4528760" } 
+        18363 { $Script:1909 = "W2K10v1909" }
+        18363 { $Script:1909x86 = "W2K10v1909" }
+        19041 { $Script:OS = "W2K10v2004" }
+        19041 { $Script:2004 = "W2K10v2004" }
+        19041 { $Script:2004x86 = "W2K10v2004" }
+        # PENDING: Server 2016
+        default { $Script:OS = "Not Listed" }
+    }
 
-#Shorten website
-$Script:Web = "http://download.windowsupdate.com/d/msdownload/update/software/secu/2020/01/windows10.0-"
+
+    #Alert user that system is currently updating
+    msg console /server:localhost "Hello , your computer needs security patches and will need to remain powered on until complete. You can continue using the system but prepare by saving data as there will be a need to reboot later"
+    Set-Variable -Name "BUILD" -Value ([Environment]::OSVersion.Version).Build
+
+    #Shorten website
+    $Script:Web = "http://download.windowsupdate.com/d/msdownload/update/software/secu/2020/01/windows10.0-"
+    #Create Variable that is Script wide
+    $Script:LINK = "EMPTY"
 
 
-
-#Determine download based on OS Build & "Bitness"
-switch ( $KB ) {
-    
-    W2K10v1507 {"email that this does not have patching" }
-    W2K10v1511 {"email that this does not have patching" }
-    
-    W2K10v1607 { if ((gwmi win32_operatingsystem | select osarchitecture).osarchitecture -eq "64-bit") {
-            #64 bit logic here
-            Write "64-bit OS Detected" | Out-file -Filepath c:\support\Updates\$env:computername.hotfixes.txt -Append
-            $Script:LINK = "LINK" -Value "$Web$KB64"
-        }
-        else {
-            #32 bit logic here
-            Write "32-bit OS Detected" | Out-file -Filepath c:\support\Updates\$env:computername.hotfixes.txt -Append
-            Set-Variable -Name "LINK" -Value "$Web$KB86"
-        }}
-  
+    #Determine download based on OS Build & "Bitness"
+    switch ( $KB ) {
+        { "email that this does not have patching" }
+        W2K10v1511 { "email that this does not have patching" }
+        KB4534271 
 
         
-        default { $Script:LINK = "LINK" -Value "ERROR"}
+        default { $Script:LINK = "ERROR" }
     }
     
 
@@ -190,7 +195,7 @@ switch ( $KB ) {
         Get-FileFromUrl $URL $Filename1
     
         $Rename = Start-Job -ScriptBlock { Rename-Item -Path "C:\Support\Updates\CumulativeUpdate.tmp" -NewName "C:\Support\Updates\$OS_$KB_CumulativeUpdate.msu"
-        $Rename | Wait-Job | Receive-Job
+            $Rename | Wait-Job | Receive-Job
  
              
         }
